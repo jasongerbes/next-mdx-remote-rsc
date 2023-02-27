79341f4327656611ca7219b6fcb23e1fbc208caf
Jason Gerbes <j.gerbes@me.com>
2023-02-27 14:43:58 +1300

    Add next-mdx-remote, update home page

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import { compileMDX } from "next-mdx-remote/rsc";
+
+export default async function Home() {
+  const { content, frontmatter } = await compileMDX({
+    source: `
+      ---
+      title: RSC Frontmatter Example
+      ---
+      # Hello World
+      This is from Server Components!
+    `,
+    options: { parseFrontmatter: true },
+  });
+  return (
+    <>
+      <h1>{frontmatter.title}</h1>
+      {content}
+    </>
+  );
+}
