603908b0ce842c1315350699b3aeabe4a5735dcb
Jason Gerbes <j.gerbes@me.com>
2023-02-27 14:53:29 +1300

    Remove extra heading

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,6 @@ export default async function Home() {
       ---
       title: RSC Frontmatter Example
       ---
-      # Hello World
       This is from Server Components!
     `,
     options: { parseFrontmatter: true },
